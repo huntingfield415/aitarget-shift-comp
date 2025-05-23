45dcac8d624cdd7eccf17d6a573a10c82f5e1261
github huntingfield415git config --global user.email karino@hunting-field.comgit config --global core.editor atom <karino@hunting-field.com>
2025-05-23 17:08:29 +0900

    追加: 年齢別保育士配置基準を定義した STAFF_RATIO を追加

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+// /lib/constants.ts
+export const STAFF_RATIO = [
+  { ageInMonthsMin: 0, ageInMonthsMax: 12, ratio: 3 },
+  { ageInMonthsMin: 13, ageInMonthsMax: 24, ratio: 6 },
+  { ageInMonthsMin: 25, ageInMonthsMax: 36, ratio: 6 },
+  { ageInMonthsMin: 37, ageInMonthsMax: 60, ratio: 20 },
+  { ageInMonthsMin: 61, ageInMonthsMax: 999, ratio: 30 },
+];
